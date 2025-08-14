d6af5ae606aeb71f6e8ee016862af6dbd0820892
Eric Mercer <egm@cs.byu.edu>
2025-08-14 22:05:27 +0000

    Udpate hw0 and reset main

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,7 @@
+mod hw1_basic_specifications;
 use vstd::prelude::*;
 
 verus! {
 
-spec fn min(x: int, y: int) -> int {
-    if x <= y {
-        x
-    } else {
-        y
-    }
-}
-
-fn foo() {
-    assert(min(10, 20) == 10);
-    assert(min(-10, -20) == -20);
-    assert(forall|i: int, j: int| min(i, j) <= i && min(i, j) <= j);
-    assert(forall|i: int, j: int| min(i, j) == i || min(i, j) == j);
-    assert(forall|i: int, j: int| min(i, j) == min(j, i));
-    // assert(forall|i: int, j: int| min(i, j) == min(i, i));
-}
-
-// Verified, but executable
-pub fn fn_min(x: i32, y: i32) -> i32 {
-    if x <= y {
-        x
-    } else {
-        y
-    }
-}
-
-fn octuple(x1: i8) -> i8 {
-    let x2 = x1 + x1;
-    let x4 = x2 + x2;
-    x4 + x4
-}
 
 } // verus!
-fn main() {
-    println!("Hello world! And goodbye.");
-    foo();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_min_exec() {
-        assert_eq!(fn_min(10, 20), 10);
-        assert_eq!(fn_min(5, -5), -5);
-    }
-}
