c29019a93fd4b2be86f8384358677b370c987975
Steve Olsen <stevelolsen@gmail.com>
2025-09-10 21:32:40 -0600

    Don't touch anything it is all working

--- docs/templates/loop_invariant_template.rs
+++ docs/templates/loop_invariant_template.rs
@@ -6,17 +6,12 @@ use vstd::prelude::*;
 
 verus! {
     pub exec fn sum_prefix(a: &Vec<u64>) -> (s: u64)
-        requires
-            // e.g., a.len() <= 1_000_000
-        ensures
-            // s is the sum of elements of a
     {
         let mut i: usize = 0;
         let mut acc: u64 = 0;
         while i < a.len()
             invariant
-                // 0 <= i <= a.len()
-                // acc equals sum of a[0..i)
+                i <= a.len(),
         {
             acc = acc + a[i];
             i = i + 1;
