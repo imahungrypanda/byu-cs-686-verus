1887dd390dc7afb24632602176b74a785c1e3d28
Steve Olsen <stevelolsen@gmail.com>
2025-09-19 18:23:21 -0600

    Adds initial structure for recursion and loops
    Sets up the initial file structure for the recursion and loops homework assignment, including placeholder `run_examples` functions for each problem.
    Removes the old homework file.
    Updates `main.rs` to import and call `run_examples` for the new homework problems.

---
+++ src/hw2_recursion_and_loops.rs
@@ -0,0 +1,27 @@
+use vstd::prelude::*;
+
+verus! {
+
+pub mod problem1 {
+
+    pub fn run_examples() {
+        assert(true);
+    }
+}
+
+pub mod problem2 {
+
+    pub fn run_examples() {
+        assert(true);
+    }
+}
+
+pub mod problem3 {
+
+    pub fn run_examples() {
+        assert(true);
+    }
+}
+
+
+} // verus!
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 // Import all homework modules
 mod hw0;
 mod hw1_basic_specifications;
-mod hw2;
+mod hw2_recursion_and_loops;
 mod hw3;
 mod hw4;
 
 mod example_1;
-mod class;
+// mod class;
 use vstd::prelude::*;
 
 verus! {
 
-
+fn class_run_examples() {
+    // class::mult::run_examples();
+    // class::binary_search_class::run_examples();
+}
 
 fn main() {
 
@@ -20,9 +23,12 @@ fn main() {
     // Run homework 1 examples
     hw1_basic_specifications::problem1::run_examples();
     hw1_basic_specifications::problem2::run_examples();
+    hw1_basic_specifications::problem3::run_examples();
 
     // Run homework 2 examples (when implemented)
-    // hw2::run_examples();
+    hw2_recursion_and_loops::problem1::run_examples();
+    hw2_recursion_and_loops::problem2::run_examples();
+    hw2_recursion_and_loops::problem3::run_examples();
 
     // Run homework 3 examples (when implemented)
     // hw3::run_examples();
@@ -32,9 +38,8 @@ fn main() {
 
     // Run reading 1 examples
     example_1::run_examples();
-    class::mult::run_examples();
 
-    class::binary_search_class::run_examples();
+    class_run_examples();
 }
 
 } // verus!
